33ac82bd268b6c4d7de30b32365ae6f2a57a4831
Elliot <me@robiot.dev>
2021-05-03 21:11:53 +0300

    Pre Alpha

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "include/lexer.h"
+#include "include/parser.h"
+#include "include/visitor.h"
+#include "include/io.h"
+
+void print_help()
+{
+    printf("Usage: shelf.exe <filename>\n");
+    exit(1);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2){
+        print_help();
+    }
+    lexer_T* lexer = init_lexer(get_file_contents(argv[1]));
+    parser_T* parser = init_parser(lexer);
+    AST_T* root = parser_parse(parser, parser->scope);
+    visitor_T* visitor = init_visitor();
+    visitor_visit(visitor, root);
+
+    return 0;
+}
